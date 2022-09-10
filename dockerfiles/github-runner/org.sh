f66364fc4262083081bf7e16bb337211f796ac55
작은모코코 <whya5448@gmail.com>
2022-09-10 18:55:43 +0900

    github-runner org-repo

---
+++ dockerfiles/github-runner/org.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+source default.env
+
+ARCH=$(uname -m)
+COMPOSE=docker-compose-mac.yml
+if [ $ARCH == 'x86_64' ]; then
+  COMPOSE=docker-compose.yml
+fi
+
+ORG_NAME=$1
+RUNNER_SCOPE=org
+DOCKER_NAME=github-runner-$ORG_NAME
+RUNNER_WORKDIR=/tmp/$DOCKER_NAME
+
+mkdir "$ORG_NAME"
+
+cat << EOF > "$ORG_NAME/repo.env"
+ORG_NAME=$ORG_NAME
+RUNNER_SCOPE=$RUNNER_SCOPE
+DOCKER_NAME=$DOCKER_NAME
+RUNNER_WORKDIR=$RUNNER_WORKDIR
+EOF
+
+cp $COMPOSE "$ORG_NAME/docker-compose.yml"
+
+ln -s ../compose.sh "$ORG_NAME/compose.sh"
